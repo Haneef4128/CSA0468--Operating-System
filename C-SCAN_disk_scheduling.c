e6c21d261e6e14b01186c94d70a79947e29b253b
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-11-14 09:24:03 +0530

    Update C-SCAN_disk_scheduling.c

--- C-SCAN_disk_scheduling.c
+++ C-SCAN_disk_scheduling.c
@@ -1,104 +1,69 @@
-// C++ program to demonstrate
-// C-SCAN Disk Scheduling algorithm
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-// Code by Vikram Chaurasia
-
-int size = 8;
-int disk_size = 200;
-
-void CSCAN(int arr[], int head)
-{
-	int seek_count = 0;
-	int distance, cur_track;
-	vector<int> left, right;
-	vector<int> seek_sequence;
-
-	// appending end values
-	// which has to be visited
-	// before reversing the direction
-	left.push_back(0);
-	right.push_back(disk_size - 1);
-
-	// tracks on the left of the
-	// head will be serviced when
-	// once the head comes back
-	// to the beginning (left end).
-	for (int i = 0; i < size; i++) {
-		if (arr[i] < head)
-			left.push_back(arr[i]);
-		if (arr[i] > head)
-			right.push_back(arr[i]);
-	}
-
-	// sorting left and right vectors
-	std::sort(left.begin(), left.end());
-	std::sort(right.begin(), right.end());
-
-	// first service the requests
-	// on the right side of the
-	// head.
-	for (int i = 0; i < right.size(); i++) {
-		cur_track = right[i];
-		// appending current track to seek sequence
-		seek_sequence.push_back(cur_track);
-
-		// calculate absolute distance
-		distance = abs(cur_track - head);
-
-		// increase the total count
-		seek_count += distance;
-
-		// accessed track is now new head
-		head = cur_track;
-	}
-
-	// once reached the right end
-	// jump to the beginning.
-	head = 0;
-
-	// adding seek count for head returning from 199 to 0
-	seek_count += (disk_size - 1);
-
-	// Now service the requests again
-	// which are left.
-	for (int i = 0; i < left.size(); i++) {
-		cur_track = left[i];
-
-		// appending current track to seek sequence
-		seek_sequence.push_back(cur_track);
-
-		// calculate absolute distance
-		distance = abs(cur_track - head);
-
-		// increase the total count
-		seek_count += distance;
-
-		// accessed track is now the new head
-		head = cur_track;
-	}
-
-	cout << "Total number of seek operations = "
-		<< seek_count << endl;
-
-	cout << "Seek Sequence is" << endl;
-
-	for (int i = 0; i < seek_sequence.size(); i++) {
-		cout << seek_sequence[i] << endl;
-	}
-}
-
-// Driver code
-int main()
-{
-
-	// request array
-	int arr[size] = { 176, 79, 34, 60, 92, 11, 41, 114 };
-	int head = 50;
-
-	cout << "Initial position of head: " << head << endl;
-	CSCAN(arr, head);
-
-	return 0;
+int main() {
+    int RQ[100], i, j, n, TotalHeadMovement = 0, initial, size, move;
+    
+    printf("Enter the number of Requests\n");
+    scanf("%d", &n);
+    
+    printf("Enter the Requests sequence\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &RQ[i]);
+    
+    printf("Enter initial head position\n");
+    scanf("%d", &initial);
+    
+    printf("Enter total disk size\n");
+    scanf("%d", &size);
+    
+    printf("Enter the head movement direction for high 1 and for low 0\n");
+    scanf("%d", &move);
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (RQ[j] > RQ[j + 1]) {
+                int temp;
+                temp = RQ[j];
+                RQ[j] = RQ[j + 1];
+                RQ[j + 1] = temp;
+            }
+        }
+    }
+    
+    int index;
+    for (i = 0; i < n; i++) {
+        if (initial < RQ[i]) {
+            index = i;
+            break;
+        }
+    }
+    
+    if (move == 1) {
+        for (i = index; i < n; i++) {
+            TotalHeadMovement = TotalHeadMovement + abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+        TotalHeadMovement = TotalHeadMovement + abs(size - 1 - RQ[index]);
+        initial = size - 1;
+        for (i = 0; i < index; i++) {
+            TotalHeadMovement = TotalHeadMovement + abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+    } else {
+        for (i = index; i >= 0; i--) {
+            TotalHeadMovement = TotalHeadMovement + abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+        TotalHeadMovement = TotalHeadMovement + abs(RQ[index]);
+        initial = 0;
+        for (i = n - 1; i > index; i--) {
+            TotalHeadMovement = TotalHeadMovement + abs(RQ[i] - initial);
+            initial = RQ[i];
+        }
+    }
+    
+    printf("Total head movement: %d\n", TotalHeadMovement);
+    
+    return 0;
 }
